9d747d6b889ad0a136217172c6da2d519243b3ec
Carlos Alabau <carlytos347@gmail.com>
2020-02-22 14:02:34 +0100

    Nuevo contenido en about

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
     $("nav").hide();
-    $(".cerrar-menu").hide();
-    $(".btn-menu").click(function(){
+    $(".cerrar-menu, .cerrar-menu-about").hide();
+
+    $(".btn-menu, .btn-menu-about").click(function(){
        $("nav").show();
-       $(".portada").css("height","90vh");
-       $(".cerrar-menu").show();
-       $(".btn-menu").hide();
+       $(".portada,.about").css("height","90vh");
+       $(".cerrar-menu, .cerrar-menu-about").show();
+       $(".btn-menu, .btn-menu-about").hide();
     })
-    $(".cerrar-menu").click(function(){
-        $(".portada").css("height","100vh");
-        $(".btn-menu").show();
-        $(".cerrar-menu").hide();
+    
+    $(".cerrar-menu, .cerrar-menu-about").click(function(){
+        $(".portada, .about").css("height","100vh");
+        $(".btn-menu, .btn-menu-about").show();
+        $(".cerrar-menu, .cerrar-menu-about").hide();
         $("nav").hide();
     })
 })
